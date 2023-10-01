4e13ffa978ddf534b58e1639571c3027b29fc8f4
Andreas Claesson <mexus81@mexus.se>
2023-10-01 23:00:35 +0200

    Meny omgjord2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-// Add an event listener for mouseenter to show the dropdown
+// Event listener for mouseenter to show the dropdown
 document.querySelector(".dropdown").addEventListener("mouseenter", function() {
     document.getElementById("myDropdown").classList.add("show");
   });
   
-  // Add an event listener for mouseleave to hide the dropdown
+  // Event listener for mouseleave to hide the dropdown
   document.querySelector(".dropdown").addEventListener("mouseleave", function() {
     document.getElementById("myDropdown").classList.remove("show");
   });
